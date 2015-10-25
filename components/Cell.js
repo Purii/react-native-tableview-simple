bcc4eba705c8771f4f0bb6103a29dd9a48fd27eb
Patrick Puritscher <blackout289@gmail.com>
2015-10-25 22:20:55 +0100

    added accessory Disclosure Indicator

--- components/Cell.js
+++ components/Cell.js
@@ -8,13 +8,22 @@ export default class Cell extends Component {
       detail: ''
     }
     const isPressable = this.props.onPress ? true : false;
+    const accessory = this.props.accessory || false;
 
+    let renderAccessory = () => {
+      if(!accessory) return;
+      console.log(accessory);
+      return (
+        <View style={styles.cell_DiIn}></View>
+      )
+    }
     /** CellStyle: Basic */
     let CellBasic = () => {
       return(
         <View style={styles.cellcontainer}>
           <View style={styles.cell}>
             <Text style={isDisabled ? [...{}, styles.cell_title, styles.cell_text__disabled] : styles.cell_title}>{title}</Text>
+            {renderAccessory()}
           </View>
       </View>
       )
@@ -27,6 +36,7 @@ export default class Cell extends Component {
           <View style={styles.cell}>
             <Text style={isDisabled ? [...{}, styles.cell_title, styles.cell_text__disabled] : styles.cell_title}>{title}</Text>
             <Text style={isDisabled ? [...{}, styles.cell_detail, styles.cell_text__disabled] : styles.cell_detail}>{detail}</Text>
+            {renderAccessory()}
           </View>
         </View>
       )
@@ -39,6 +49,7 @@ export default class Cell extends Component {
           <View style={styles.cell}>
             <Text style={isDisabled ? [...{}, styles.cell_leftdetail, styles.cell_text__disabled] : styles.cell_leftdetail}>{detail}</Text>
             <Text style={isDisabled ? [...{}, styles.cell_title, styles.cell_text__disabled] : styles.cell_title}>{title}</Text>
+            {renderAccessory()}
           </View>
         </View>
       )
@@ -49,8 +60,11 @@ export default class Cell extends Component {
       return(
         <View style={styles.cellcontainer}>
           <View style={styles.cell__subtitle}>
-            <Text style={isDisabled ? [...{}, styles.cell_title, styles.cell_text__disabled] : styles.cell_title}>{title}</Text>
-            <Text style={isDisabled ? [...{}, styles.cell_subtitle, styles.cell_text__disabled] : styles.cell_subtitle}>{detail}</Text>
+            <View style={styles.cellinner__subtitle}>
+              <Text style={isDisabled ? [...{}, styles.cell_title, styles.cell_text__disabled] : styles.cell_title}>{title}</Text>
+              <Text style={isDisabled ? [...{}, styles.cell_subtitle, styles.cell_text__disabled] : styles.cell_subtitle}>{detail}</Text>
+            </View>
+            {renderAccessory()}
           </View>
         </View>
       )
@@ -103,11 +117,16 @@ var styles = StyleSheet.create({
     alignItems: 'center'
   },
   'cell__subtitle': {
-    flexDirection: 'column',
     paddingLeft: 15,
     paddingRight: 20,
     paddingTop: 10,
     paddingBottom: 10,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  'cellinner__subtitle': {
+    flexDirection: 'column',
+    flex: 1
   },
   'cell_title':{
     fontSize: 16,
@@ -132,4 +151,17 @@ var styles = StyleSheet.create({
   'cell_text__disabled':{
     color: 'gray'
   },
+
+  'cell_DiIn': {
+    width: 10,
+    height: 10,
+    marginLeft: 5,
+    backgroundColor: 'transparent',
+    borderTopWidth: 1.5,
+    borderRightWidth: 1.5,
+    borderColor: '#c7c7cc',
+    transform: [{
+      rotate: '45deg'
+    }]
+  }
 });
