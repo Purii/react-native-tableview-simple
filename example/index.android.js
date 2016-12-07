4a41c4fefd83a694d81bd99bd286e75b3e75a3fd
Patrick Puritscher <blackout289@gmail.com>
2016-12-07 20:26:09 +0100

    updated example to RN 0.39.0

--- example/index.android.js
+++ example/index.android.js
@@ -6,48 +6,119 @@
 
 import React, { Component } from 'react';
 import {
+  ActivityIndicator,
   AppRegistry,
+  Dimensions,
+  ScrollView,
   StyleSheet,
+  Switch,
   Text,
-  View
+  View,
 } from 'react-native';
 
-export default class example extends Component {
+import {
+  Cell,
+  CustomCell,
+  Section,
+  TableView,
+} from 'react-native-tableview-simple';
+
+// Example component for section:headerComponent
+const CustomSectionHeader = () => (
+  <View>
+    <Text>Custom header!</Text>
+  </View>
+);
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Example extends Component {
+  // eslint-disable-next-line class-methods-use-this
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <ScrollView contentContainerStyle={styles.stage}>
+        <TableView>
+          <Section header="STANDARD" footer="A Footer">
+            <Cell cellStyle="Basic" title="Basic" />
+            <Cell cellStyle="RightDetail" title="RightDetail" detail="Detail" />
+            <Cell cellStyle="LeftDetail" title="LeftDetail" detail="Detail" />
+            <Cell cellStyle="Subtitle" title="Subtitle" detail="No linebreakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" />
+            <Cell cellStyle="Basic" title="Pressable w/ accessory" accessory="DisclosureIndicator" onPress={() => console.log('Heyho!')} />
+          </Section>
+          <Section header="DISABLED">
+            <Cell cellStyle="Basic" isDisabled title="Basic" />
+            <Cell cellStyle="RightDetail" isDisabled title="RightDetail" detail="Detail" />
+            <Cell cellStyle="LeftDetail" isDisabled title="LeftDetail" detail="Detail" />
+            <Cell cellStyle="Subtitle" isDisabled title="Subtitle" detail="No linebreakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" />
+            <Cell cellStyle="Basic" isDisabled title="Pressable w/ accessory" accessory="DisclosureIndicator" onPress={() => console.log('Heyho!')} />
+          </Section>
+          <Section header="ACCESSORY">
+            <Cell cellStyle="Basic" accessory="DisclosureIndicator" title="Basic" />
+            <Cell cellStyle="RightDetail" accessory="DetailDisclosure" title="RightDetail" detail="Detail" />
+            <Cell cellStyle="LeftDetail" accessory="Detail" title="LeftDetail" detail="Detail" />
+            <Cell cellStyle="Subtitle" accessory="Checkmark" title="Subtitle" detail="No linebreakkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk" />
+            <Cell cellStyle="Basic" accessory="Detail" title="Pressable w/ accessory" onPress={() => console.log('Heyho!')} />
+          </Section>
+          <Section header="MISC">
+            <Cell cellStyle="RightDetail" title="RightDetail" detail="Detail" rightDetailColor="#6cc644" />
+            <Cell cellStyle="LeftDetail" title="LeftDetail" detail="Detail" leftDetailColor="#6cc644" />
+          </Section>
+          <Section header="CUSTOMCELLS">
+            <CustomCell onPress={() => console.log('Heyho!')} contentContainerStyle={{ height: 44 }}>
+              <Text style={{ flex: 1, fontSize: 16 }}>Loading</Text>
+              <ActivityIndicator />
+            </CustomCell>
+            <CustomCell onPress={() => console.log('Heyho!')} contentContainerStyle={{ height: 44 }}>
+              <Text style={{ flex: 1, fontSize: 16 }}>Switch</Text>
+              <Switch />
+            </CustomCell>
+            <CustomCell onPress={() => console.log('Heyho!')} contentContainerStyle={{ height: 60 }}>
+              <Text style={{ flex: 1, fontSize: 16 }}>Custom height</Text>
+            </CustomCell>
+          </Section>
+          <Section headerComponent={<CustomSectionHeader />}>
+            <Cell onPress={() => console.log('Heyho!')} cellStyle="Basic" title="Section uses prop headerComponent" />
+          </Section>
+        </TableView>
+        <View
+          style={{
+            minHeight: Dimensions.get('window').height,
+          }}
+        >
+          <View
+            style={{
+              backgroundColor: '#37474F',
+              height: 500,
+              alignItems: 'center',
+              justifyContent: 'center',
+            }}
+          >
+            <View
+              style={{
+                backgroundColor: '#ffc107',
+                width: 80,
+                height: 80,
+                borderRadius: 10,
+              }}
+            />
+          </View>
+          <TableView>
+            <Section footer="All rights reserved.">
+              <Cell title="Help / FAQ" titleTextColor="#007AFF" onPress={() => console.log('open Help/FAQ')} />
+              <Cell title="Contact Us" titleTextColor="#007AFF" onPress={() => console.log('open Contact Us')} />
+            </Section>
+          </TableView>
+        </View>
+      </ScrollView>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  stage: {
+    backgroundColor: '#EFEFF4',
+    paddingTop: 20,
+    paddingBottom: 20,
   },
 });
 
-AppRegistry.registerComponent('example', () => example);
+AppRegistry.registerComponent('example', () => Example);
