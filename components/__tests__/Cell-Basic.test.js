804aa7a892436244b04441b9c242b2d172a4ac1e
PDGP <blackout289@gmail.com>
2019-07-06 12:33:02 +0200

    update tests to test for testID

--- components/__tests__/Cell-Basic.test.js
+++ components/__tests__/Cell-Basic.test.js
@@ -11,6 +11,13 @@ it('renders basic', () => {
   expect(tree).toMatchSnapshot();
 });
 
+it('renders with testID', () => {
+  const tree = renderer
+    .create(<Cell cellStyle="Basic" title="Basic" testID="testID" />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
 it('renders with accessory', () => {
   const tree = renderer
     .create(
