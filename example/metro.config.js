b545fc56a724fc424a2d1920cd4d568551eeb3c6
PDGP <blackout289@gmail.com>
2019-10-16 23:56:54 +0200

    refactor: use actual components as example dep

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,7 +5,33 @@
  * @format
  */
 
+const path = require('path');
+
+// react-native >= 0.57
+
 module.exports = {
+  resolver: {
+    extraNodeModules: new Proxy(
+      /* The first argument to the Proxy constructor is passed as
+       * "target" to the "get" method below.
+       * Put the names of the libraries included in your reusable
+       * module as they would be imported when the module is actually used.
+       */
+      {
+        'react-native-tableview-simple': path.resolve(__dirname, '../src/'),
+      },
+      {
+        get: (target, name) => {
+          if (target.hasOwnProperty(name)) {
+            return target[name];
+          }
+          return path.join(process.cwd(), `node_modules/${name}`);
+        },
+      },
+    ),
+  },
+  watchFolders: [path.resolve(__dirname, '../src')],
+  projectRoot: path.resolve(__dirname),
   transformer: {
     getTransformOptions: async () => ({
       transform: {
--- index.js
+++ index.js
@@ -1,4 +1,1 @@
-export { default as Cell } from './components/Cell';
-export { default as Section } from './components/Section';
-export { default as Separator } from './components/Separator';
-export { default as TableView } from './components/TableView';
+export { Cell, Section, Separator, TableView } from './src';
