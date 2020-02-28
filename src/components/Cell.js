ee62100bc9d21846a421cf0616ec4916de82db7b
katerynapalchykova <katerynapalchykova@gmail.com>
2020-02-28 10:45:55 -0600

    Update Cell.js

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -200,7 +200,7 @@ const Cell = props => {
         allowFontScaling={allowFontScaling}
         numberOfLines={1}
         style={_styles.cell_title}
-        {...titleTextProps || {}}>
+        {...titleTextProps}>
         {title}
       </Text>
     </View>
@@ -216,7 +216,7 @@ const Cell = props => {
         allowFontScaling={allowFontScaling}
         numberOfLines={1}
         style={_styles.cell_title}
-        {...titleTextProps || {}}>
+        {...titleTextProps}>
         {title}
       </Text>
       <Text
@@ -227,7 +227,7 @@ const Cell = props => {
             ? [_styles.cell_rightDetail, _styles.cell_text__disabled]
             : _styles.cell_rightDetail
         }
-        {...detailTextProps || {}}>
+        {...detailTextProps}>
         {detail}
       </Text>
     </View>
@@ -247,14 +247,14 @@ const Cell = props => {
             ? [_styles.cell_leftDetail, _styles.cell_text__disabled]
             : _styles.cell_leftDetail
         }
-        {...detailTextProps || {}}>
+        {...detailTextProps}>
         {detail}
       </Text>
       <Text
         allowFontScaling={allowFontScaling}
         numberOfLines={1}
         style={_styles.cell_leftDetailTitle}
-        {...titleTextProps || {}}>
+        {...titleTextProps}>
         {title}
       </Text>
     </View>
@@ -272,7 +272,7 @@ const Cell = props => {
           allowFontScaling={allowFontScaling}
           numberOfLines={1}
           style={_styles.cell_title}
-          {...titleTextProps || {}}>
+          {...titleTextProps}>
           {title}
         </Text>
         <Text
@@ -283,7 +283,7 @@ const Cell = props => {
               ? [_styles.cell_subtitle, _styles.cell_text__disabled]
               : _styles.cell_subtitle
           }
-          {...detailTextProps || {}}>
+          {...detailTextProps}>
           {detail}
         </Text>
       </View>
