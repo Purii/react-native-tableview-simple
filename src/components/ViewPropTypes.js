685a0e85c4e5bd6e6888b82ee590cde640818403
顾铭峰 <16450382@qq.com>
2020-03-20 17:26:10 +0800

    eslint fix

--- src/components/ViewPropTypes.js
+++ src/components/ViewPropTypes.js
@@ -1,5 +1,5 @@
 import { ViewPropsTypes as RNViewPropTypes, View } from 'react-native';
 
-const ViewPropTypes  = RNViewPropTypes || View.propTypes || {};
+const ViewPropTypes = RNViewPropTypes || View.propTypes || {};
 
 export default ViewPropTypes;
